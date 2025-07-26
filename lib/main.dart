65359233c2791e931f707352ac63793141581c9d
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-07-26 20:25:17 +0530

    added color palette

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: HomePage(),
     );
   }
