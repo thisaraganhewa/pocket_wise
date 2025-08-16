b0ad50a0e7fdc2bb769850c0354eecb55f21f083
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-08-16 18:35:28 +0530

    added fire base auth and fire store and google sig in packages

--- lib/data/datasources/user_remote_data_source.dart
+++ lib/data/datasources/user_remote_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class UserRemoteDataSource {
 
@@ -9,6 +11,8 @@ abstract class UserRemoteDataSource {
 class UserRemoteDataSourceImpl implements UserRemoteDataSource{
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn  _googleSignIn = GoogleSignIn.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Future<User> logOrRegister() async{
