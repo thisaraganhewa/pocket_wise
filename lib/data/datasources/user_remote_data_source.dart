0f92fd8c8e875af40a7bf0c9c8e37895b2905361
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-11-02 22:50:25 +0530

    implememted the handleGetAuthcode

--- lib/data/datasources/user_remote_data_source.dart
+++ lib/data/datasources/user_remote_data_source.dart
@@ -117,12 +117,15 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource {
 
     if (response.statusCode != 200) {
       if (response.statusCode == 401 || response.statusCode == 403) {
-        _isAuthorized = false;//if responce status not qualse to 200 and the status code is 401 or 403 is authorization will be set to false
+        _isAuthorized =
+            false; //if responce status not qualse to 200 and the status code is 401 or 403 is authorization will be set to false
         _errorMessage =
             'People API gave a ${response.statusCode} response. '
-            'Please re-authorize access.';//assign thses message to error message after the request is failed
+            'Please re-authorize access.'; //assign thses message to error message after the request is failed
       } else {
-        print('People API ${response.statusCode} response: ${response.body}');//if there is status code other than 401 or 403 .. like 500 this section will 
+        print(
+          'People API ${response.statusCode} response: ${response.body}',
+        ); //if there is status code other than 401 or 403 .. like 500 this section will
         _contactText =
             'People API gave a ${response.statusCode} '
             'response. Check logs for details.';
@@ -132,46 +135,73 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource {
     final Map<String, dynamic> data =
         json.decode(response.body) as Map<String, dynamic>;
 
-    final String? namedContact = _pickFirstNameContact(data);//gets the first name of the contacts
+    final String? namedContact = _pickFirstNameContact(
+      data,
+    ); //gets the first name of the contacts
 
-    if(namedContact != null){//if the name is null shows no contacts founf if not it ll show the contact
+    if (namedContact != null) {
+      //if the name is null shows no contacts founf if not it ll show the contact
       _contactText = 'I see you know $namedContact!';
-    }
-    else{
+    } else {
       _contactText = 'No contacts to display.';
     }
   }
 
-  String? _pickFirstNameContact(Map<String, dynamic> data){
-    final List<dynamic>? connections = data['connections'] as List<dynamic>?;//extract the connection list which have the contacts from the JSON
-    final Map<String, dynamic>? contact = connections?.firstWhere((dynamic contact) => (contact as Map<Object?, dynamic>)['names'] != null, orElse: () => null,) as Map<String, dynamic>?;
+  String? _pickFirstNameContact(Map<String, dynamic> data) {
+    final List<dynamic>? connections =
+        data['connections']
+            as List<
+              dynamic
+            >?; //extract the connection list which have the contacts from the JSON
+    final Map<String, dynamic>? contact =
+        connections?.firstWhere(
+              (dynamic contact) =>
+                  (contact as Map<Object?, dynamic>)['names'] != null,
+              orElse: () => null,
+            )
+            as Map<String, dynamic>?;
 
-    if(contact != null){
+    if (contact != null) {
       final List<dynamic> names = contact['names'] as List<dynamic>;
-      final Map<String, dynamic>? name = names.firstWhere((dynamic name) => (name as Map<Object?, dynamic>)['displayName'] != null, orElse: () => null) as Map<String, dynamic>?;
+      final Map<String, dynamic>? name =
+          names.firstWhere(
+                (dynamic name) =>
+                    (name as Map<Object?, dynamic>)['displayName'] != null,
+                orElse: () => null,
+              )
+              as Map<String, dynamic>?;
 
-      if(name != null){
+      if (name != null) {
         return name['displayName'] as String?;
       }
-
-    } 
+    }
     return null;
   }
 
   Future<void> _handleAuthorizationScopes(GoogleSignInAccount user) async {
-    try{
-      final GoogleSignInClientAuthorization? authorization = await user.authorizationClient.authorizationForScopes(scopes);
+    try {
+      final GoogleSignInClientAuthorization? authorization = await user
+          .authorizationClient
+          .authorizationForScopes(scopes);
 
       authorization;
 
       _isAuthorized = true;
       _errorMessage = '';
-    }
-    on GoogleSignInException catch(e){
+    } on GoogleSignInException catch (e) {
       _errorMessage = _errorMessageFromSignInException(e);
     }
-    
   }
-  
 
+  Future<void> _handleGetAuthCode(GoogleSignInAccount user) async {
+    try {
+      final GoogleSignInServerAuthorization? serverAuth = await user
+          .authorizationClient
+          .authorizeServer(scopes);
+
+      _serverAuthCode = serverAuth == null ? '' : serverAuth.serverAuthCode;
+    } on GoogleSignInException catch (e) {
+      _errorMessage = _errorMessageFromSignInException(e);
+    }
+  }
 }
