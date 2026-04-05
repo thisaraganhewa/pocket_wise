1d17ef9d3d8cacabc8fa3024899ee2ce66863488
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-04-05 08:09:22 +0530

    added Auth bloc sign with google method

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -3,14 +3,29 @@ import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:pocket_wise/features/auth/domain/entities/user_entity.dart';
+import 'package:pocket_wise/features/auth/domain/usecases/sign_in_with_google_use_case.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+
+  final SignInWithGoogleUseCase signInWithGoogleUseCase;
+
+  AuthBloc(this.signInWithGoogleUseCase) : super(AuthInitial())  {
+    on<SignInWithGoogleEvent>((event, emit) async {
+
+      emit(AuthLoading());
+
+      try{
+        final user = await signInWithGoogleUseCase.logOrRegister();
+
+        emit(AuthSuccess(user!));
+
+      }
+      catch(e){
+        emit(AuthError(e.toString()));
+      }
     });
   }
 }
