56e7f0fadbe9794357674e2522469189522c475c
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-04-05 08:35:43 +0530

    added auth bloc and sign in with google in dependencey injection

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -4,14 +4,19 @@ import 'package:pocket_wise/features/auth/data/datasources/user_remote_data_sour
 import 'package:pocket_wise/features/auth/data/repository/user_repository_impl.dart';
 import 'package:pocket_wise/features/auth/domain/repositories/user_repository.dart';
 import 'package:pocket_wise/features/auth/domain/usecases/sign_in_with_google_use_case.dart';
+import 'package:pocket_wise/features/auth/presentation/bloc/auth_bloc.dart';
 
 final sl = GetIt.instance;
 
 
 Future<void> init() async{
 
+  sl.registerFactory(() => AuthBloc(sl()));
+
   sl.registerLazySingleton<UserRemoteDataSource>(() => UserRemoteDataSourceImpl());
   sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(sl()));
   sl.registerLazySingleton<SignInWithGoogleUseCase>(() => SignInWithGoogleUseCase(sl()));
+  sl.registerLazySingleton(() => SignInWithGoogleUseCase(sl()));
+  
 
 }
