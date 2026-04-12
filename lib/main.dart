7ade4db1c62093e19d77e577c8681216c9d2c378
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-04-12 07:48:00 +0530

    feat: added auth block in main and added dash boead page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pocket_wise/core/di/injection_container.dart' as di;
+import 'package:pocket_wise/features/auth/domain/usecases/sign_in_with_google_use_case.dart';
+import 'package:pocket_wise/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:pocket_wise/firebase_options.dart';
 import 'package:pocket_wise/features/auth/presentation/pages/home_page.dart';
 import 'package:pocket_wise/features/auth/presentation/pages/login_page.dart';
@@ -20,9 +23,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: LoginPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(create: (_) => AuthBloc(SignInWithGoogleUseCase(di.sl())))
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        home: LoginPage(),
+      ),
     );
   }
 }
