7aa465baa64e2628a49fbbf47e4a2d62c6528596
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-31 07:37:08 +0530

    feat: initialize AppTextStyles with headlineXl style

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+
 class AppTextStyles {
-  
+  static const TextStyle headlineXl = TextStyle(
+    fontFamily: 'HankenGrotesk',
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    height: 56 / 48,
+    letterSpacing: -0.96
+  );
 }
