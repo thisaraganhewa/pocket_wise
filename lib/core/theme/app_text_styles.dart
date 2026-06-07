620e6e0c25aa5e218d23fe4a2a97a101be79392c
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-06-07 20:33:06 +0530

    feat: implement Hanken Grotesk text styles for application typography

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -8,4 +8,28 @@ class AppTextStyles {
     height: 56 / 48,
     letterSpacing: -0.96
   );
+
+  static const TextStyle headlineXlMobile = TextStyle(
+    fontFamily: 'HankenGrotesk',
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    height: 40 / 32,
+    letterSpacing: -0.64
+  );
+
+  static const TextStyle headlineLg = TextStyle(
+    fontFamily: 'HankenGrotesk',
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    height: 32 / 24
+  );
+
+  static const TextStyle bodyLg = TextStyle(
+    fontFamily: 'HankenGrotesk',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 24 / 16
+  );
+
+
 }
